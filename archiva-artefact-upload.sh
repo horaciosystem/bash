4f840ec230056be95606dfe9d753d4dfb95aa6b7
horaciosystem <horacioalexandre.fernandes@gmail.com>
2014-10-09 09:46:35 -0300

    implemented to upload in snpshots or internal repository, according pom file version

--- archiva-artefact-upload.sh
+++ archiva-artefact-upload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-remote_url="http://archiva.systextil.com.br:9090/archiva/repository/internal/"
 
 
 cd /home/horacio/Documents/temp/systextil
@@ -10,6 +9,14 @@ for i in $(find . -name *.pom); do
  artifactId=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='artifactId']/text()"  $i)
  BASEDIR=$(dirname $i)
  file="$BASEDIR/$artifactId-$version.jar"
+
+ repository="internal/"
+ if [[ $version == *SNAPSHOT* ]]
+ then
+  repository="snapshots/";
+ fi
+
+ remote_url="http://archiva.systextil.com.br:9090/archiva/repository/$repository"
 
  mvn deploy:deploy-file -DgroupId=$groupId -DartifactId=$artifactId -Dpackaging=jar -Dversion=$version -Dfile=$file -DgeneratePom=true -DrepositoryId=internal -Durl=${remote_url} 
  
