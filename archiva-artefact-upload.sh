f065bf2e9b41601bb8d881d50e35331f690584d4
horaciosystem <horacioalexandre.fernandes@gmail.com>
2014-10-09 09:06:46 -0300

    finish script to upload artefacts to archiva external repository! GREAT!

--- archiva-artefact-upload.sh
+++ archiva-artefact-upload.sh
@@ -5,14 +5,13 @@ remote_url="http://archiva.systextil.com.br:9090/archiva/repository/internal/"
 cd /home/horacio/Documents/temp/systextil
 
 for i in $(find . -name *.pom); do
- version=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()"  $i) #arquivo pom
+ version=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()"  $i)
  groupId=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='groupId']/text()"  $i)
  artifactId=$(xmllint --xpath "//*[local-name()='project']/*[local-name()='artifactId']/text()"  $i)
- file=
- 
+ BASEDIR=$(dirname $i)
+ file="$BASEDIR/$artifactId-$version.jar"
+
+ mvn deploy:deploy-file -DgroupId=$groupId -DartifactId=$artifactId -Dpackaging=jar -Dversion=$version -Dfile=$file -DgeneratePom=true -DrepositoryId=internal -Durl=${remote_url} 
  
 done
 
- 
-# Apache Commons Codec
-mvn deploy:deploy-file -DgroupId=commons-codec -DartifactId=commons-codec -Dpackaging=jar -Dversion=1.6 -Dfile="${dependency_dir}/commons-codec-1.6.jar" -DgeneratePom=true -DrepositoryId=internal -Durl=${remote_url}
